335304390d4fcb10da64559bd25cb335b83ffa26
muzaffer <muzaffer.deveci@tazebt.com>
2021-09-06 01:26:31 +0300

    uyelik 1

---
+++ MovieM.Client/Areas/Identity/Data/MovieMClientUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace MovieM.Client.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the MovieMClientUser class
+    public class MovieMClientUser : IdentityUser
+    {
+    }
+}
---
+++ MovieM.Client/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MovieM.Client.Areas.Identity.Data;
+using MovieM.Client.Data;
+
+[assembly: HostingStartup(typeof(MovieM.Client.Areas.Identity.IdentityHostingStartup))]
+namespace MovieM.Client.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                //services.AddDbContext<MovieMClientContext>(options =>
+                //    options.UseSqlServer(
+                //        context.Configuration.GetConnectionString("MovieMClientContextConnection")));
+
+                //services.AddDefaultIdentity<MovieMClientUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                //    .AddEntityFrameworkStores<MovieMClientContext>();
+            });
+        }
+    }
+}
